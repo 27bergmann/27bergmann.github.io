10ae7114a53563aad21c23db0ee2212c3b15208d
27bergmann <manfacescript@gmail.com>
2025-12-22 21:48:47 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('copy-btn').addEventListener('click', () => {
-  const scriptText = document.getElementById('script-url').value;
-  navigator.clipboard.writeText(scriptText).then(() => {
-    alert('Teks berhasil disalin ke clipboard!');
-  }).catch(err => {
-    alert('Gagal menyalin teks: ' + err);
-  });
-});
